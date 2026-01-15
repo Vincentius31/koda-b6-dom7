62449a1fecdd32182b0d34caf5e73e97e7b16696
KODA <vincentius85@gmail.com>
2026-01-15 14:55:22 +0700

    feat: adding the fetch data with image character and name of the character on display

---
+++ script.js
@@ -0,0 +1,43 @@
+const container = document.getElementById("characters");
+const searchInput = document.getElementById("search");
+
+let allCharacters = [];
+
+async function loadAllCharacters() {
+  let page = 1;
+  let totalPages = 1;
+
+  while (page <= totalPages) {
+    const res = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+    const data = await res.json();
+
+    totalPages = data.info.pages;
+    allCharacters = allCharacters.concat(data.results);
+
+    page++;
+  }
+
+  renderCharacters(allCharacters);
+}
+
+function renderCharacters(data) {
+  container.innerHTML = "";
+
+  data.forEach(char => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const img = document.createElement("img");
+    img.src = char.image;
+    img.alt = char.name;
+
+    const name = document.createElement("p");
+    name.textContent = char.name;
+
+    card.appendChild(img);
+    card.appendChild(name);
+    container.appendChild(card);
+  });
+}
+
+loadAllCharacters();
